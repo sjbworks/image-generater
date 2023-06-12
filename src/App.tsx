481e20d52471d8f3c1c6375fd88b106ae078197a
sjbworks <sacred.jizo.bosatz@gmail.com>
2023-06-13 00:29:58 +0900

    wip: get hex from image

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useRef, useMemo } from "react";
+import reactLogo from "./assets/react.svg";
+import sampleImage from "./assets/IMG20230110184534.jpg";
+import "./App.css";
+import Vibrant from "node-vibrant";
+
+const getColors = async () => {
+  const colors = await Vibrant.from(
+    "src/assets/IMG20230110184534.jpg"
+  ).getPalette();
+  // const colorHexes = Object.keys(colors)
+  //   .map((key) => colors[key]?.hex)
+  //   .sort();
+  console.log(colors);
+};
+
+interface Color {
+  vibrant: string | null;
+  muted: string | null;
+  // 他の色相についても必要に応じて追加できます
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const ref = useRef<HTMLImageElement>(null);
+  const [colors, setColors] = useState<Color | null>(null);
+  useMemo(
+    () =>
+      Vibrant.from("src/assets/IMG20230110184534.jpg")
+        .getPalette()
+        .then((palette) => {
+          const extractedColors: Color = {
+            vibrant: palette.Vibrant?.hex || null,
+            muted: palette.Muted?.hex || null,
+          };
+
+          setColors(extractedColors);
+        }),
+    []
+  );
+  console.log(colors);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+        <img src={sampleImage} className="logo" alt="Sample Image" ref={ref} />
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
@@ -29,7 +61,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
